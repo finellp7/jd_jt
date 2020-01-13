9cddaf12b3bb454a7afa96ad7b78416f2d69aaf3
llp <1249945708@qq.com>
2020-01-13 21:11:23 +0800

    首次添加

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// // 控制顶端搜索栏的显示与隐藏
+// window.onscroll=function(){
+// 	// console.log('页面啊加载啦')
+// 	// 获取滚动的距离
+// 	var top=document.documentElement.scrollTop;
+// 	console.log(top);
+
+//     // 获取页面顶部的div class=form
+//     var form=document.getElementsByClassName('form')[0];
+//     // 假如top为150时，form出现了
+//     if(top>=150){
+//     	form.style.position='fixed';
+//     	form.style.top="0";
+//     	nav.style.height="48px";
+//     	nav.style.display="block";
+//     	nav.style.zIndex=1000;
+
+//     }else{
+//     	form.style.display="none";
+//     	form.style.height="0";
+//     }
+ 
+// }
